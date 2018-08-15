839a9bd9a096c2870c2a994d57ffefc8d4f41dc8
silence1772 <603497448@qq.com>
2018-08-15 07:01:10 -0700

    update some detail

--- v0.08/tcpserver.cpp
+++ v0.08/tcpserver.cpp
@@ -9,7 +9,7 @@ TcpServer::TcpServer(EventLoop* loop, const InetAddress& addr)
 	  name_(addr.ToHostPort()),
 	  acceptor_(new Acceptor(loop, addr)),
 	  is_started_(false),
-	  next_connid_(-1)
+	  next_connid_(0)
 {
 	acceptor_->SetNewConnectionCallback(std::bind(&TcpServer::HandleNewConnection, this, std::placeholders::_1, std::placeholders::_2));
 }
@@ -33,10 +33,9 @@ void TcpServer::HandleNewConnection(int sockfd, const InetAddress& peer_addr)
 {
 	loop_->AssertInBirthThread();
 	char buf[32];
-	snprintf(buf, sizeof(buf), "#%d", next_connid_);
-	++next_connid_;
+	snprintf(buf, sizeof(buf), "#%d", next_connid_++);
 	std::string conn_name = name_ + buf;
-	LOG_INFO << "TcpServer::HandleNewConnection [" << conn_name << "] from " << peer_addr.ToHostPort();
+	LOG_INFO << "new connection [" << conn_name << "] from " << peer_addr.ToHostPort();
 
 	InetAddress local_addr(socketops::GetLocalAddr(sockfd));
 	TcpConnectionPtr conn(new TcpConnection(loop_, conn_name, sockfd, local_addr, peer_addr));
